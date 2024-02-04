fef681385844c935fc38105a2b59cbd19cdbe40d
lsilencej <43839502+lsilencej@users.noreply.github.com>
2024-02-05 01:15:17 +0800

    fix: xfsprogs build failed

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,2 +1,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# fix xfsprogs
+sed -i 's/TARGET_CFLAGS += -DHAVE_MAP_SYNC/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/' feeds/packages/utils/xfsprogs/Makefile
